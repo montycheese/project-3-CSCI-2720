6ec04b56b7d15075c3c564109c549eed91618807
Neo-Hao <neohao@uga.edu>
2015-11-13 17:33:01 -0800

    sort structure

---
+++ sort.cpp
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include "sortFunction.h"
+
+
+int mode = -1;
+int * theArray;
+int length;
+
+
+int sortFunction(int * data, int mode = 1) {
+	if (mode == 1) {
+		// insertion sort
+		insertionSort(data);
+	} 
+	if (mode == 2) {
+		// merge sort
+		mergeSort(data);
+	}
+
+	if (mode == 3) {
+		// quick sort
+		quickSort(data);
+	}
+
+	return 0;
+}
+
+
+int main(int argc, char *argv[]) {
+	if (argv[1][0] == '-') {
+		// determine the mode
+		if (argv[1][1] == 'i') {
+			// insertion sort
+			mode = 1;
+		} else if (argv[1][1] == 'm') {
+			// merge sort
+			mode = 2;
+		} else if (argv[1][1] == 'q') {
+			// quick sort
+			mode = 3;
+		} else {
+			// wrong modes
+			printf("Your input is not valid. There are only three modes: -i, -m, and -q.\n");
+			return 0;
+		}
+
+		// get the numbers given through argv[]
+		length = argc - 2;
+		theArray = (int *) calloc(length, sizeof(int));
+
+		for (int i = 0; i < length; i++) {
+			try {
+				sscanf(argv[i+2],"%d", &theArray[i]);
+			} 
+			catch (int e) {
+				printf("The number you provided is not an integer. We only accept integers.\n");
+				free(theArray);
+				return 0;
+			}
+		}
+
+		// sort and report
+		sortFunction(theArray, mode);
+		free(theArray);
+
+	} else {
+		// no modes
+		printf("Your input is not valid. Try something like ./sort -i 33 12 0 -2 1\n");
+	}
+
+	return 0;
+}
---
+++ sortFunction.h
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+
+//insertion
+int insertionSort(int * data) {
+
+}
+
+// merge
+int mergeSort(int * data) {
+
+}
+
+// quick
+int quickSort(int * data) {
+
+}
